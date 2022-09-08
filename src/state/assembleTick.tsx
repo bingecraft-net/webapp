0ac75ae7b7936386c29c04484f14596f943e17f8
tyler <tyler@cruftbusters.com>
2022-09-08 15:42:20 -0500

    refactor assembleTick

--- src/state/assembleTick.tsx
+++ src/state/assembleTick.tsx
@@ -6,6 +6,9 @@ export default function assembleTick({ machines }: State): State {
   return {
     machines: machines.map((machine): Machine => {
       if (machine.type !== 'assembler') return machine
+      const recipe = settings.recipes.find(
+        (recipe) => recipe.key === machine.recipeKey,
+      )!
 
       const potential = Math.min(20, machine.potential + 1)
       if (potential < 20) return { ...machine, potential }
@@ -13,36 +16,25 @@ export default function assembleTick({ machines }: State): State {
       const outCountByName = stackCountByName(machine.outStacks)
       const inCountByName = stackCountByName(machine.inStacks)
 
-      const recipe = settings.recipes.find(
-        (recipe) => recipe.key === machine.recipeKey,
-      )!
-      if (
-        recipe.ingredients.every(
-          (ingredient) =>
-            (inCountByName.get(ingredient.name) || 0) >= ingredient.count,
-        )
-      ) {
-        recipe.ingredients.forEach((ingredient) =>
-          inCountByName.set(
-            ingredient.name,
-            inCountByName.get(ingredient.name)! - ingredient.count,
-          ),
-        )
-        recipe.products.forEach((product) =>
-          outCountByName.set(
-            product.name,
-            (outCountByName.get(product.name) || 0) + product.count,
-          ),
-        )
-        return {
-          ...machine,
-          inStacks: stacks(inCountByName),
-          outStacks: stacks(outCountByName),
-          potential: 0,
-        }
+      for (const ingredient of recipe.ingredients) {
+        const available = inCountByName.get(ingredient.name) || 0
+        if (available < ingredient.count) return { ...machine, potential }
+        inCountByName.set(ingredient.name, available - ingredient.count)
       }
 
-      return { ...machine, potential }
+      for (const product of recipe.products) {
+        outCountByName.set(
+          product.name,
+          (outCountByName.get(product.name) || 0) + product.count,
+        )
+      }
+
+      return {
+        ...machine,
+        inStacks: stacks(inCountByName),
+        outStacks: stacks(outCountByName),
+        potential: 0,
+      }
     }),
   }
 }
