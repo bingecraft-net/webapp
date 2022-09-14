071a7b0ed6b333d0af193d2169aaba576f3324d9
tyler <tyler@cruftbusters.com>
2022-09-14 13:12:03 -0700

    add minecraftservers.org link

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -27,17 +27,25 @@ export default function App() {
       <div>
         <h2 style={{ margin: 0 }}>links</h2>
         <p style={{ margin: '0.5rem 0' }}>
-          <Link href="https://servers-minecraft.net/server-bingecraft-net.22146">
-            servers-minecraft.net
-          </Link>
-          <Link href="mailto:glizzywort@gmail.com">
-            email (glizzywort@gmail.com)
-          </Link>
           <Link href="https://pbs.twimg.com/media/FFdKHA7X0AQCkVv?format=jpg&name=4096x4096">
             ore distribution
           </Link>
           <Link href="https://sordid-nightshade-1c3.notion.site/bingecraft-net-3a044ab1b24949e2a5058f025cb63152">
             notion.so
+          </Link>
+          <Link href="mailto:glizzywort@gmail.com">
+            email (glizzywort@gmail.com)
+          </Link>
+        </p>
+      </div>
+      <div>
+        <h2 style={{ margin: 0 }}>server lists</h2>
+        <p style={{ margin: '0.5rem 0' }}>
+          <Link href="https://servers-minecraft.net/server-bingecraft-net.22146">
+            servers-minecraft.net
+          </Link>
+          <Link href="https://minecraftservers.org/server/641560">
+            minecraftservers.org
           </Link>
         </p>
       </div>
