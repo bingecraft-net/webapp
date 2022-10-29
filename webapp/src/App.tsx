12d9056094cebe4e54bc615d0adef9687cfc56ee
tyler <tyler@cruftbusters.com>
2022-10-29 13:28:40 -0700

    update description and simplify outline

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,49 +1,38 @@
 import React from 'react'
 
-const darkBlue = '#224'
-
 export default function App() {
   return (
     <div style={{ padding: '1rem' }}>
-      <h1
-        style={{
-          margin: 0,
-          fontWeight: 'bold',
-          fontSize: '4rem',
-          color: darkBlue,
-        }}
-      >
-        bingecraft.net
-      </h1>
-      <div>
-        <h2 style={{ margin: 0 }}>welcome</h2>
-        <p style={{ margin: '0.5rem 0' }}>
-          Welcome to Glizzy’s Marinade! We like to build and make friends.
-          We have a new, very small server with grief response, looting
-          prevention, skills, dynmap. We don’t have any warps so the town
-          is very intimate. Come join our growing community and help us
-          build a cozy autumn town! I’m your server operator Glizzy and I’m
-          a big geek who understands running a server. I enforce the rules
-          (no griefing, no looting, no being a douchebag). I hope to meet
-          you soon!
-        </p>
-      </div>
-      <div>
-        <h2 style={{ margin: 0 }}>links</h2>
-        <p style={{ margin: '0.5rem 0' }}>
-          <Link href="https://map.bingecraft.net/">map</Link>
-          <Link href="https://pbs.twimg.com/media/FFdKHA7X0AQCkVv?format=jpg&name=4096x4096">
-            ore distribution
-          </Link>
-          <Link href="https://discord.gg/SShpb4S2wq">discord</Link>
-          <Link href="mailto:glizzywort@gmail.com">
-            email (glizzywort@gmail.com)
-          </Link>
-          <Link href="https://www.planetminecraft.com/server/bingecraft-net/">
-            our planetminecraft page
-          </Link>
-        </p>
-      </div>
+      <h1>glizzy's marinade smp</h1>
+      <h3>easy-going community, cute town, harsh wilderness</h3>
+      <p style={{ margin: '0.5rem 0' }}>
+        Welcome to Glizzy’s Marinade SMP! We are a small community with
+        grief response, looting prevention, skills, and dynmap. Every third
+        night a bloodmoon will raise the difficulty in the spawn region
+        from peaceful to hard. The nether, the end, and the highlands are
+        permanently hard difficulty! Come join our growing community and
+        build your house in the cozy autumn town!
+      </p>
+      <p style={{ margin: '0.5rem 0' }}>
+        I’m your server operator, Glizzy. I’m a big geek and I enjoy
+        writing code, running servers, and building towns. I enforce the
+        rules (no griefing, no looting, no being a douchebag). Find me
+        in-game!
+      </p>
+      <h2>links</h2>
+      <p>
+        <Link href="https://map.bingecraft.net/">map</Link>
+        <Link href="https://pbs.twimg.com/media/FFdKHA7X0AQCkVv?format=jpg&name=4096x4096">
+          ore distribution
+        </Link>
+        <Link href="https://discord.gg/SShpb4S2wq">discord</Link>
+        <Link href="mailto:glizzywort@gmail.com">
+          email (glizzywort@gmail.com)
+        </Link>
+        <Link href="https://www.planetminecraft.com/server/bingecraft-net/">
+          our planetminecraft page
+        </Link>
+      </p>
     </div>
   )
 }
@@ -56,7 +45,7 @@ function Link({ children, href }: LinkProps) {
     <a
       style={{
         textDecoration: 'none',
-        color: darkBlue,
+        color: 'inherit',
         fontWeight: 'bold',
         display: 'block',
       }}
